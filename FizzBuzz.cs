56ff93095923816e9636f65e803d7934afb77f88
Mark Haskamp <mark.haskamp@pinnsg.com>
2013-06-25 14:02:28 -0400

    initial load

---
+++ FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string FizzBuzzerate(int n) {
+            var returnString = new StringBuilder("");
+
+            if (n%3 == 0) {
+                returnString.Append("fizz");
+            }
+
+            if (n%5 == 0) {
+                returnString.Append("buzz");
+            }
+
+            if (returnString.Length > 0) {
+                return returnString.ToString();
+            }
+
+            return n.ToString();
+        }
+    }
+}
---
+++ test/FizzBuzzTest/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FizzBuzzTest
+{
+    public class Class1
+    {
+        private global::FizzBuzz.FizzBuzz fb;
+
+        [SetUp]
+        public void SetUp() {
+            fb = new global::FizzBuzz.FizzBuzz();
+        }
+
+        [Test]
+        public void Mod_1_returns_n() {
+            Assert.AreEqual("1", fb.FizzBuzzerate(1));
+        }
+
+        [Test]
+        public void divisible_by_3_means_fizz() {
+            Assert.AreEqual("fizz", fb.FizzBuzzerate(3));
+        }
+
+        [Test]
+        public void divisible_by_5_means_buzz() {
+            Assert.AreEqual("buzz", fb.FizzBuzzerate(5));
+        }
+
+        [Test]
+        public void divisible_by_3_and_5_means_fizzbuzz_15() {
+            Assert.AreEqual("fizzbuzz", fb.FizzBuzzerate(15));
+        }
+
+        [Test]
+        public void divisible_by_3_and_5_means_fizzbuzz_30() {
+            Assert.AreEqual("fizzbuzz", fb.FizzBuzzerate(30));
+        }
+    }
+}
